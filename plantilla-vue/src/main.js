adbafeea4339a22f17bf2fdd276ad2ae74e42ec5
Bsatián Santana <bastian.santana@usach.cl>
2017-09-06 18:58:15 -0300

    commit final

--- plantilla-vue/src/main.js
+++ plantilla-vue/src/main.js
@@ -4,13 +4,11 @@ import Index from './Index.vue';
 import Actors from './Actors.vue';
 import NuevoActor from './NuevoActor.vue';
 import VueResource from 'vue-resource';
-//import VueValidator from 'vue-form-validator';
 require("./style.scss");
 
 import App from './App.vue';
 Vue.use(VueRouter);
 Vue.use(VueResource);
-//Vue.use(VueValidator);
 const routes = [
   { path: '/index', alias: '/', component: Index},
   { path: '/actors', component: Actors},
