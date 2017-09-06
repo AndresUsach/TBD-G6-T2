af4f838c36884491654753aec8014488b7a1efc1
Bsatián Santana <bastian.santana@usach.cl>
2017-09-06 18:27:06 -0300

    Validación de campos

--- plantilla-vue/src/NuevoActor.vue
+++ plantilla-vue/src/NuevoActor.vue
@@ -6,15 +6,15 @@
       <label for="nombre">
         Nombre
       </label><br>
-      <input id="nombre" type="text" v-model="actor.firstName" /><br>
+      <input id="nombre" required type="text" v-model="actor.firstName" required /><br>
       <label for="apellido">Apellido</label><br>
-      <input id="apellido" type="text" v-model="actor.lastName" /><br>
+      <input id="apellido" required type="text" v-model="actor.lastName" required /><br>
       <button v-on:click="post">Agregar actor</button>
     </form>
     <div v-if="submitted">
       <h2>¡Actor añadido!</h2>
       <!--Añadir otro actor
-      <button v-on:click="">Agregar otro actor</button>-->
+      <button v-on:click="refresh">Agregar otro actor</button>-->
     </div>
     </ul>
   </div>
@@ -35,15 +35,18 @@ export default {
   methods:{
     //Metodo para agregar actores
     post: function(){
-      this.$http.post('http://localhost:8081/sakila-spring-backend/actors', {
-        firstName: this.actor.firstName,
-        lastName: this.actor.lastName
-      }).then(function(data){
-        console.log(data);
-        this.submitted = true;
-      });
+      //Si los campos no se encuentran vacios se agrega el actor
+      if(this.actor.firstName != '' && this.actor.lastName != ''){
+        this.$http.post('http://localhost:8081/sakila-spring-backend/actors', {
+          firstName: this.actor.firstName,
+          lastName: this.actor.lastName
+        }).then(function(data){
+          console.log(data);
+          this.submitted = true;
+        });
+      }
     }
-  }
+}
 
 }
 </script>
